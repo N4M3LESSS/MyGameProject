fb5febcd5db1078e39bab547f6cb7dc4f35323fa
N4M3LESSS <mrkkoollo@gmail.com>
2024-10-11 10:15:49 +0300

    lab02

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "game.h"
+
+int main() {
+Game::Game(){}
+void Game::start(){}
+void Game::displayBoard(){}
+bool Game::checkWin(){}
+return 0;
+}
---
+++ player.h
@@ -0,0 +1,8 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+enum Player {
+	EMPTY,
+	PLAYER_X,
+	PLAYER_O
+};
